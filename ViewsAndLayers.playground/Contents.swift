4aafd52b4d13686bdc53cd4ee643e677265e2901
Ben Chatelain <ben@octop.ad>
2018-08-26 11:32:27 -0600

    🚧Add WIP view code

--- ViewsAndLayers.playground/Contents.swift
+++ ViewsAndLayers.playground/Contents.swift
@@ -23,20 +23,51 @@ fileprivate extension Double {
 //: ### The main ButtonView class
 
 class ButtonView: UIView {
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    configureLayers()
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    configureLayers()
-  }
-  
-  private func configureLayers() {
-    backgroundColor = #colorLiteral(red: 0.9600390625, green: 0.9600390625, blue: 0.9600390625, alpha: 1)
-    // TODO
-  }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLayers()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureLayers()
+    }
+
+    private func configureLayers() {
+        backgroundColor = #colorLiteral(red: 0.9600390625, green: 0.9600390625, blue: 0.9600390625, alpha: 1)
+        // TODO
+    }
+
+    private lazy var greenBackground: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.fillColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+
+        return layer
+    }()
+
+    private lazy var inProgressLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+
+        return layer
+    }
+
+    private lazy var badgeLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.colors = [] as [CGColor]
+        layer.frame = layer.bounds
+        layer.applyPopShadow()
+        layer.
+
+        return layer
+    }()
+
+    private func createBadgeLayerMask() -> CAShapeLayer {
+        let scale = layer.bounds.width / UIBezierPath.badgePath.bounds.width
+        let mask = CAShapeLayer()
+        mask.path = UIBezierPath.badgePath.cgPath
+        mask.transform = CATransform3DMakeScale(scale, scale, 1)
+        return mask
+    }
 }
 
 //: ### Present the button
